53311615b51ec905bd94029735bb5a766eff600f
PKTseng <jk0910948659@gmail.com>
2024-08-29 22:03:08 +0800

    update useState

--- 04-react-state-basics/02-define-state/src/App.js
+++ 04-react-state-basics/02-define-state/src/App.js
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  // let count = 0;
-
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   function increase() {
-    count++;
+    setCount(count + 1)
   }
 
   return (
@@ -15,6 +13,6 @@ function App() {
       <h1>{count}</h1>
       <button onClick={increase}>增加</button>
     </div>
-  );
+  )
 }
-export default App;
+export default App
