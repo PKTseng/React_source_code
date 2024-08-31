e3bb35a6b9ca15bd9a06938a77ec419dc52b1204
PKTseng <jk0910948659@gmail.com>
2024-08-31 15:08:16 +0800

    update array list

--- 04-react-state-basics/04-update-array-state/src/App.js
+++ 04-react-state-basics/04-update-array-state/src/App.js
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [list, setList] = useState([1, 2, 3, 4]);
+  const [list, setList] = useState([1, 2, 3, 4])
 
-  function handleAdd() {
-    setList([...list, list.length + 1]);
-  }
-
-  function handlePlusTen(index) {
-    const newList = [...list];
-    newList[index] += 10;
-    setList(newList);
-  }
-
-  function handleDelete(item) {
-    setList(list.filter((v) => v !== item));
+  const handleAdd = () => {
+    setList([...list, list.length + 1])
   }
 
   return (
@@ -23,13 +13,12 @@ function App() {
       <ul>
         {list.map((item, index) => (
           <li key={index}>
-            {item} <button onClick={() => handlePlusTen(index)}>+10</button>{" "}
-            <button onClick={() => handleDelete(item)}>删除此项</button>
+            {item} <button>+10</button>{' '}
           </li>
         ))}
       </ul>
-      <button onClick={handleAdd}>添加</button>
+      <button onClick={handleAdd}>新增陣列</button>
     </div>
-  );
+  )
 }
-export default App;
+export default App
