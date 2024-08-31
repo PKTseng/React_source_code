cc43357a09a5996e4fc6a2d0792f27931ef2b174
PKTseng <jk0910948659@gmail.com>
2024-08-31 15:43:56 +0800

    刪除物件屬性

--- 04-react-state-basics/05-update-object-state/src/App.js
+++ 04-react-state-basics/05-update-object-state/src/App.js
@@ -1,32 +1,15 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
   const [person, setPerson] = useState({
-    name: "张三",
+    name: 'ken',
     age: 25,
-    nest: {
-      a: 1,
-    },
-  });
+  })
 
-  function handleAdd() {
-    setPerson({
-      ...person,
-      gender: "男",
-    });
-  }
-
-  function handleChangeAge() {
-    setPerson({
-      ...person,
-      age: 30,
-    });
-  }
-
-  function handleRemoveAge() {
-    const { age, ...newPerson } = person;
-    setPerson(newPerson);
+  const deleteAge = () => {
+    const { age, ...newPerson } = person
+    setPerson(newPerson)
   }
 
   return (
@@ -38,10 +21,8 @@ function App() {
           </li>
         ))}
       </ul>
-      {/* <button onClick={handleAdd}>添加性别</button> */}
-      {/* <button onClick={handleChangeAge}>修改年龄</button> */}
-      <button onClick={handleRemoveAge}>删除年龄</button>
+      <button onClick={deleteAge}>Delete Age</button>
     </div>
-  );
+  )
 }
-export default App;
+export default App
