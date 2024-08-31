ec385170d7152892baf5b092a9002be6fc0a52b9
PKTseng <jk0910948659@gmail.com>
2024-08-31 15:53:55 +0800

    深入淺拷貝

--- 04-react-state-basics/05-update-object-state/src/App.js
+++ 04-react-state-basics/05-update-object-state/src/App.js
@@ -3,24 +3,48 @@ import './App.css'
 
 function App() {
   const [person, setPerson] = useState({
-    name: 'ken',
-    age: 25,
+    name: 'Alice',
+    age: 30,
+    address: {
+      city: 'New York',
+      country: 'USA',
+      postalCode: '10001',
+    },
+    hobbies: ['reading', 'swimming'],
   })
 
   const deleteAge = () => {
-    const { age, ...newPerson } = person
-    setPerson(newPerson)
+    const {
+      address: { postalCode, ...addressRest },
+      ...personRest
+    } = person
+    const newPerson = { ...personRest, address: addressRest }
+
+    console.log(newPerson)
+
+    //   {
+    //     "name": "Alice",
+    //     "age": 30,
+    //     "hobbies": [
+    //         "reading",
+    //         "swimming"
+    //     ],
+    //     "address": {
+    //         "city": "New York",
+    //         "country": "USA"
+    //     }
+    // }
   }
 
   return (
     <div className="container">
-      <ul>
+      {/* <ul>
         {Object.keys(person).map((key, index) => (
           <li key={index}>
             {key}: {person[key]}
           </li>
         ))}
-      </ul>
+      </ul> */}
       <button onClick={deleteAge}>Delete Age</button>
     </div>
   )
