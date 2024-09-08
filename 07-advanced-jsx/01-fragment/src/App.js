a3f9e05b4842ebfe69f94616fa9571b4c56a5b8d
PKTseng <jk0910948659@gmail.com>
2024-09-08 14:52:36 +0800

    use Fragment

--- 07-advanced-jsx/01-fragment/src/App.js
+++ 07-advanced-jsx/01-fragment/src/App.js
@@ -1,31 +1,23 @@
-import { Fragment } from "react";
-import "./App.css";
+import { Fragment } from 'react'
+import './App.css'
 
 function App() {
-  const tags = ["React", "Vue", "JavaScript"];
+  const tags = ['React', 'Vue', 'JavaScript']
 
   return (
     <main className="container">
       <User />
-      {tags.map((tag) => {
-        return (
-          <Fragment key={tag}>
-            <span>{tag}</span>
-            <hr />
-          </Fragment>
-        );
-      })}
     </main>
-  );
+  )
 }
 
 function User() {
   return (
-    <>
-      <p>用户名：张三</p>
-      <p>职业：前端工程师</p>
-    </>
-  );
+    <Fragment>
+      <p>User Name : ken</p>
+      <p>Job Title : front end</p>
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
