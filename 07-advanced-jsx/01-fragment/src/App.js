58b4a19cd3ffebaa7bc60fc145b42c68762e309f
PKTseng <jk0910948659@gmail.com>
2024-09-08 15:18:43 +0800

    use params component

--- 07-advanced-jsx/01-fragment/src/App.js
+++ 07-advanced-jsx/01-fragment/src/App.js
@@ -4,29 +4,37 @@ import './App.css'
 function App() {
   const tags = ['React', 'Vue', 'JavaScript']
 
-  return (
-    <main className="container">
-      <User />
-
-      {tags.map((tag) => {
-        return (
-          <Fragment key={tag}>
-            <p>{tag}</p>
-            <hr />
-          </Fragment>
-        )
-      })}
-    </main>
-  )
-}
-
-function User() {
-  return (
+  const Head = (
     <>
       <p>User Name : ken</p>
       <p>Job Title : front end</p>
     </>
   )
+
+  const tagSection = tags.map((tag) => {
+    return (
+      <Fragment key={tag}>
+        <p>{tag}</p>
+        <hr />
+      </Fragment>
+    )
+  })
+
+  return (
+    <main className="container">
+      {Head}
+      {tagSection}
+    </main>
+  )
 }
+
+// function User() {
+//   return (
+//     <>
+//       <p>User Name : ken</p>
+//       <p>Job Title : front end</p>
+//     </>
+//   )
+// }
 
 export default App
