cbe27fe80a32b6bc34e88d0692772b47ee3ae728
PKTseng <jk0910948659@gmail.com>
2024-09-08 14:17:56 +0800

    watch refresh

--- 06-react-side-effects/01-wrong-example-to-use-effects/src/App.js
+++ 06-react-side-effects/01-wrong-example-to-use-effects/src/App.js
@@ -4,17 +4,23 @@ import './App.css'
 function App() {
   const [dateTime, setDateTime] = useState(new Date())
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      setDateTime(new Date())
-    }, 1000)
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setDateTime(new Date())
+  //   }, 1000)
 
-    console.log(id)
-  }, [])
+  //   console.log(id)
+  // }, [])
+
+  const [refresh, setRefresh] = useState(0)
+  useEffect(() => {
+    setDateTime(new Date())
+  }, [refresh])
 
   return (
     <main className="container">
       <h1>{dateTime.toLocaleString('zh-CN')}</h1>
+      <button onClick={() => setRefresh(refresh + 1)}>add</button>
     </main>
   )
 }
